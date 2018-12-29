283547155e7b3179d3a576610f1699f5d30d3bf5
CarlBye <carl.pilgriim@gmail.com>
2018-12-29 18:10:57 +0800

    modify struct

--- packet.h
+++ packet.h
@@ -2,24 +2,16 @@
 #define _packet_H
 
 #define MAXDATAELEN 256
-#define PORT 5684
+
+typedef enum { REQUEST, RESPONSE, INSTRUCT } packetType;
 typedef enum { TIME, NAME, LIST, MESSAGE } requestType;
 typedef enum { CORRECT, WRONG } responseType;
 typedef enum { FORWARD } instructType;
 
 typedef struct request_packet {
-	requestType type;
+	packetType pType;
+	int type;
 	char data[MAXDATAELEN]; 
-} requestPacket;
-
-typedef struct response_packet {
-	responseType type;
-	char data[MAXDATAELEN]; 
-} responsePacket;
-
-typedef struct instruct_packet {
-	instructType type;
-	char data[MAXDATAELEN]; 
-} instructPacket;
+} packet;
 
 #endif
